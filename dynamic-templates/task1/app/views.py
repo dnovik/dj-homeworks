7be41534ef7b660b4290f1458d9f46e0ad93c082
dnovik <dnovik01@gmail.com>
2019-08-06 15:52:25 +0300

    Первая задача решена

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -33,26 +33,3 @@ def inflation_view(request):
 
     return render(request, template_name,
                   context=context)
-
-
-
-
-
-'''values = []
-
-with open(file_path, encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file)
-    headers = reader.fieldnames[0].split(';')
-    for line in reader:
-        for value in line.values():
-            values.append(value.split(';'))
-
-for i, value in enumerate(values):
-    for num, row in enumerate(value):
-        try:
-            if num == 0:
-                value[num] = str(row)
-            else:
-                value[num] = float(row)
-        except ValueError:
-            value[num] = row'''
