0ab71fd78031117f42fa954739d1047eb45d9ab2
dnovik <dnovik01@gmail.com>
2019-08-13 08:42:23 +0300

    Initial

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,8 +1,9 @@
 import csv
+from datetime import datetime
+from slugify import slugify
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -16,5 +17,12 @@ class Command(BaseCommand):
             next(phone_reader)
 
             for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+                unit = Phone()
+                unit.uid = line[0]
+                unit.name = line[1]
+                unit.image = line[2]
+                unit.price = line[3]
+                unit.release_date = datetime.date(datetime.strptime(line[4], "%Y-%m-%d"))
+                unit.lte_exists = line[5]
+                unit.slug = slugify(unit.name)
+                unit.save()
