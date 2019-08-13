0bd2727b78e135ba3a46715036661612449bebed
dnovik <dnovik01@gmail.com>
2019-08-13 12:36:14 +0300

    Initial

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from phones.models import Phone
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    if request.GET.get('sort') == 'name':
+        phone_set = Phone.objects.order_by('name')
+        context = {'phones' : phone_set}
+    elif request.GET.get('sort') == 'min_price':
+        phone_set = Phone.objects.order_by('price')
+        context = {'phones' : phone_set}
+    elif request.GET.get('sort') == 'max_price':
+        phone_set = Phone.objects.order_by('-price')
+        context = {'phones' : phone_set}
+    else:
+        phone_set = Phone.objects.all()
+        context = {'phones' : phone_set}
+        print(context)
+    
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    for phone in Phone.objects.all():
+        if phone.slug == slug:
+            context = {'phone' : phone}
     return render(request, template, context)
