12c6b94c3f139b561f99b188e362a569bfa02abe
dnovik <dnovik01@gmail.com>
2019-08-21 16:45:38 +0300

    Initial

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Category, ArticleByCategory
 
 
-@admin.register(Article)
+class ArticleByCategoryInline(admin.TabularInline):
+
+    model = ArticleByCategory
+    extra = 1
+
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+
+    inlines = [ArticleByCategoryInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    inlines = [ArticleByCategoryInline]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
+
--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='Категория')
+    
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
 
@@ -7,6 +19,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    categories = models.ManyToManyField(Category, related_name='articles', through='ArticleByCategory')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +27,10 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ArticleByCategory(models.Model):
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_main_category = models.BooleanField(verbose_name='Основной раздел')
